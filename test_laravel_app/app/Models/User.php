f34eede248fac86b39e9e9bd5541e504026019be
Sonic972 <133213712+Sonic972@users.noreply.github.com>
2025-11-10 06:42:27 +0100

    Création et modification models: users, chat, chatPaticipants et chatMessages

--- test_laravel_app/app/Models/User.php
+++ test_laravel_app/app/Models/User.php
@@ -12,17 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $table = "users";
 
+    //protected $fillable = ["id"];
+    protected $guarded = ['id'];
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -30,15 +23,10 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    //public function chats(): HasMany 
+    public function chats() {
+        return $this->hasMany(Chat::class, 'created_by');
+    }
 }
